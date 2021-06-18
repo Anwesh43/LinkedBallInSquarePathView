ddee5931a9130d19c547f878bade1891b26b92ec
Anwesh43 <anweshthecool0@gmail.com>
2021-06-19 03:15:21 +0530

    adding Int and Float extension methods

--- ballnsquarepathview/src/main/java/com/example/ballnsquarepathview/BallInSquarePathView.kt
+++ ballnsquarepathview/src/main/java/com/example/ballnsquarepathview/BallInSquarePathView.kt
@@ -26,3 +26,7 @@ val deg : Float = 90f
 val strokeFactor : Float = 90f
 val rFactor : Float = 11.2f
 val boxFactor : Float = 3.4f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
