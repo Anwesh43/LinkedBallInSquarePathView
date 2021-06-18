f96051c3f75791a96c7ed3d5c539fedbad34fe93
Anwesh43 <anweshthecool0@gmail.com>
2021-06-19 03:41:47 +0530

    adding BallInSquarePath class

--- ballnsquarepathview/src/main/java/com/example/ballnsquarepathview/BallInSquarePathView.kt
+++ ballnsquarepathview/src/main/java/com/example/ballnsquarepathview/BallInSquarePathView.kt
@@ -170,4 +170,27 @@ class BallInSquarePathView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BallInSquarePath(var i : Int) {
+
+        private var curr : BISPNode = BISPNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
