3bbbcd52a9bd33df5f3caa6d0dc269856679832c
Anwesh43 <anweshthecool0@gmail.com>
2021-06-19 03:26:26 +0530

    adding BallInSquarePathView class

--- ballnsquarepathview/src/main/java/com/example/ballnsquarepathview/BallInSquarePathView.kt
+++ ballnsquarepathview/src/main/java/com/example/ballnsquarepathview/BallInSquarePathView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawBISPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBallInSquare(scale, w, h, paint)
 }
+
+class BallInSquarePathView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
