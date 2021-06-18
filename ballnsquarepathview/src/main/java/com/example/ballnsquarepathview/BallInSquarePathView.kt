31645967057cc57c876cb9574b59a32adefab028
Anwesh43 <anweshthecool0@gmail.com>
2021-06-19 03:39:49 +0530

    adding BISPNode class

--- ballnsquarepathview/src/main/java/com/example/ballnsquarepathview/BallInSquarePathView.kt
+++ ballnsquarepathview/src/main/java/com/example/ballnsquarepathview/BallInSquarePathView.kt
@@ -129,4 +129,45 @@ class BallInSquarePathView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BISPNode(var i : Int, val state : State = State()) {
+
+        private var next : BISPNode? = null
+        private var prev : BISPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BISPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBISPNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BISPNode {
+            var curr : BISPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
